e29cff28cba3f159f38ad4989622876c60f888c5
AlexDomas <aleksej.domas@mail.ru>
2023-03-29 21:56:55 +0300

    ErrorPage when student enter wrong url

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import {Route, BrowserRouter} from "react-router-dom";
 import {Navigate, Routes} from "react-router";
 import LoginPage from "./page/LoginPage/LoginPage";
 import MembersPage from "./page/MembersPage/MembersPage";
+import ErrorPage from "./page/ErrorPage/ErrorPage";
 import './App.css';
 
 function App() {
-  
+
   const [currentStudent, setCurrentStudent] = useState({
     id: 0,
     name: "",
@@ -23,6 +24,7 @@ function App() {
           <Route path="/login" element={<LoginPage setStudent={setStudent}/>}/>
             <Route path={"/members"} element={<MembersPage currentStudent={currentStudent}/>}/>
             <Route path="/" element={<Navigate to={'/login'}/>}/>
+          <Route path="*" element={<ErrorPage/>}/>
         </Routes>
       </BrowserRouter>
   );
---
+++ src/page/ErrorPage/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../style/error.css';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1 className="error-message">You have landed on a non-existent page.</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
