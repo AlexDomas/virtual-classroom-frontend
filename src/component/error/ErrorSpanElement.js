50785b1e700160d55e2d5a2b48644fc195f1d887
AlexDomas <aleksej.domas@mail.ru>
2023-03-30 12:08:31 +0300

    change size of error message

--- src/component/error/ErrorSpanElement.js
+++ src/component/error/ErrorSpanElement.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ErrorSpanElement = ({errorMessage}) => {
     return (
-        <span style={{fontSize: "14px", color: "#00FA9A"}}>
+        <span style={{fontSize: "16px", color: "#00FA9A"}}>
            {errorMessage.name.length > 0 ?
                <div>{errorMessage.name}</div> : <div>{errorMessage.internalServerError}</div>
            }
